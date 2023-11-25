9efdcbb673682669321a02936b0fbc291d8a2993
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2023-11-25 09:14:12 +0530

    Created Quiz Cards

--- src/components/core/Dashboard/Quizes.jsx
+++ src/components/core/Dashboard/Quizes.jsx
@@ -1,30 +1,64 @@
+import "./Quizzes.css"
+
 function Quizes() {
-    const quiz = 
-    {
-        title : "HTML QUIZ",
-        description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
-        level : 'Beginner',
-        lessionNumber : 6,
-        price : 999
-    }
+    const quiz = [
+        {
+            title: "CSS Fundamentals Quiz",
+            instructor: "Rahul Sir",
+        },
+        {
+            title: "JavaScript Basics Quiz",
+            instructor: "Anita Ma'am",
+        },
+        {
+            title: "ReactJS Introduction Quiz",
+            instructor: "Vikas Sir",
+        },
+        {
+            title: "Web Design Principles Quiz",
+            instructor: "Neha Ma'am",
+        },
+        {
+            title: "NodeJS Overview Quiz",
+            instructor: "Amit Sir",
+        },
+        {
+            title: "Advanced HTML Techniques Quiz",
+            instructor: "Sonia Ma'am",
+        },
+        {
+            title: "Advanced HTML Techniques Quiz",
+            instructor: "Sonia Ma'am",
+        },
+    ]
+    
 
-
-    return <div>
-        <div className='card' style={{
-            width: "400px",
-            height: "max-content",
-            margin: "1rem",
-            padding: "1rem",
-            display: "flex",
-            flexDirection: 'column',
-            background: "white"
-        }}>
-            <h1 style={{color: "yellow"}}>
-                Hello
-                {quiz.title}
-            </h1>
-        </div>
+    return <div className="cards" style={{
+        paddingTop: "50px"
+    }}>
+{       quiz.map((q, index) => (
+                <div 
+                    key={index} 
+                    className='card' 
+                    style={{
+                        width: "400px",
+                        height: "max-content",
+                        margin: "1rem",
+                        padding: "1rem",
+                        display: "flex",
+                        flexDirection: 'column',
+                        background: "white"
+                    }}>
+                    <h1 style={{color: "black"}}>
+                        Hello {q.title}
+                    </h1>
+                    <button className="btn-red">
+                        Open Quiz
+                    </button>
+                </div>
+            ))}
     </div>
 }
+
 
 export default Quizes;
